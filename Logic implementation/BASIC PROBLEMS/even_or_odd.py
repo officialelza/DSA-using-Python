c193ca16d0678010c0d3ea264449e76786d7ff61
elza <officialelza0@gmail.com>
2025-01-03 21:48:00 +0530

    first commit

--- Logic implementation/BASIC PROBLEMS/even_or_odd.py
+++ Logic implementation/BASIC PROBLEMS/even_or_odd.py
@@ -1,7 +1,11 @@
 def is_even(n):
-    return(True if (n%2==0) else False)
+    return("Even" if (n%2==0) else "Odd")
 
+def bool_is_even(n):
+    return("Even" if ((n&1) == 0) else "Odd")
 
 num  = int(input("Enter a number : "))
+bool_num = int(input("Enter a boolean : "))
 print(f"Number is {is_even(num)}")
+print(f"Bool Number is {bool_is_even(bool_num)}")
 
